8f99d13098e47002b0aef7a443b9d9610a490020
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-25 19:19:49 -0600

    Create model

---
+++ FormulaCuadraticaApp/Models/FormulaCuadratica.cs
@@ -0,0 +1,10 @@
+﻿
+namespace FormulaCuadraticaApp.Models
+{
+    public class FormulaCuadratica
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+    }
+}
