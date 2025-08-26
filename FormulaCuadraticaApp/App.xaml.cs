42623ab97f80dd7812f9e89b4decc4da8bdf3019
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-25 19:19:30 -0600

    Create view

--- FormulaCuadraticaApp/App.xaml.cs
+++ FormulaCuadraticaApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FormulaCuadraticaApp
+﻿using FormulaCuadraticaApp.Views;
+
+namespace FormulaCuadraticaApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Main();
         }
     }
 }
---
+++ FormulaCuadraticaApp/Views/Main.xaml.cs
@@ -0,0 +1,9 @@
+namespace FormulaCuadraticaApp.Views;
+
+public partial class Main : ContentPage
+{
+	public Main()
+	{
+		InitializeComponent();
+	}
+}
